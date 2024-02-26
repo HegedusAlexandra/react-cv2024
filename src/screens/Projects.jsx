246fec312bb99d15304d0b18c1fb4d1a203048da
HegedusAlexandra <alexandra.hegedus@vamosoft.hu>
2024-02-26 11:39:01 +0100

    added project

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function Projects() {
   return (
     <div className="flex flex-col w-[100%] h-[200vh] gap-[10px] md:px-[30%] px-[4%] py-[4%] md:py-0 md:bg-transparent bg-stone-200">
-      <a href="https://leohunts.com/" className="flex-1 mb-[6vh]">
-        <div className="w-[100%] h-[70%] bg-LH bg-top bg-cover rounded-2xl "></div>
+      <a href="https://hegedusalexandra.github.io/weather_forecast/frontend/" className="flex-1 mb-[6vh]">
+        <div className="w-[100%] h-[70%] bg-WA bg-top bg-cover rounded-2xl "></div>
         <div className="w-[100%] h-[30%]">
           <h2 className="font-opensans uppercase font-bold mt-[2vh]">
-            LEOHUNTS
+            Weather App
           </h2>
-          <p>valami leirás az oldalhoz majd utána názek</p>
+          <p>School project made with JavaScript, if you type the name of the city and hi enter rovies the actual datas about it</p>
         </div>
       </a>
       <a href="https://leohunts.com/" className="flex-1 mb-[6vh]">
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         'landing': "url('./assets/pictures/landing.jpg')",
         'LH': "url('./assets/pictures/LH.png')",
         'GF': "url('./assets/pictures/GF.png')",
+        'WA': "url('./assets/pictures/WA.png')",
         'abstract': "url('./assets/pictures/abstract.jpg')",
         'lemoncoconut': "url('./assets/pictures/lemon_coconut.jpg')"
       },
