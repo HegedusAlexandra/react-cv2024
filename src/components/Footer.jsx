7b981eca73419e3abdd2aac1f903c338f37314fa
HegedusAlexandra <alexandra.hegedus@vamosoft.hu>
2024-04-18 19:57:47 +0200

    corr

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer({ id }) {
 
   return (
     <footer id={id} className="flex flex-col justify-center items-center bg-[#EAE7DC] w-[100%] px-[4%] h-[32vh] -translate-y-[4vh]">
-      <h2 className="md:text-[16vh] text-[4vh] font-bold text-cyan-500" style={window.innerWidth > 780 ? { textShadow: "1px 1px 1px rgba(0, 0, 0, 0.8)" } : {textShadow: '2px 2px 1px rgba(0, 0, 0, 0.6)'}}>Alexandra's Portfolio</h2>
+      <h2 className="md:text-[14vh] text-[3vh] font-bold text-cyan-500" style={window.innerWidth > 780 ? { textShadow: "1px 1px 1px rgba(0, 0, 0, 0.8)" } : {textShadow: '2px 2px 1px rgba(0, 0, 0, 0.6)'}}>Alexandra's Portfolio</h2>
       <p className="pb-[1vh]">&copy; Copyright 2024 Hegedüs Alexandra</p>
     </footer>
   );
